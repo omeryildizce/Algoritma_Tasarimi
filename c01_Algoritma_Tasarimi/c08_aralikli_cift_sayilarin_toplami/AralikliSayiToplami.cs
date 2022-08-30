9bface7c66a9513e4e304580357a99dada03490a
omeryildizce <omeryildizce@gmail.com>
2022-08-30 19:10:32 +0300

    Aralıklı çift sayıların toplamı

---
+++ c01_Algoritma_Tasarimi/c08_aralikli_cift_sayilarin_toplami/AralikliSayiToplami.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c08_aralikli_cift_sayilarin_toplami
+{
+    public class AralikliSayiToplami
+    {
+        public static int ciftSayi(int sayi1, int sayi2)
+        {
+            int toplam1 = 0;
+            int toplam2 = 0;
+            int n = 0;
+            if (sayi1 %2 ==0)
+            {
+                sayi1 = sayi1 - 1;
+            }
+                   
+            n = sayi1 / 2;
+            toplam1 = n*(n+1) ;
+            if (sayi2%2==1)
+            {
+                sayi2 = sayi2 - 1;
+            }
+           n = sayi2 / 2 ;
+            toplam2 = n*(n+1) ;
+            return toplam2 - toplam1;
+        }
+    }
+}
---
+++ c01_Algoritma_Tasarimi/c08_aralikli_cift_sayilarin_toplami/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c08_aralikli_cift_sayilarin_toplami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ilk_deger = 7;
+            int son_deger = 9;
+
+            Console.WriteLine(AralikliSayiToplami.ciftSayi(ilk_deger, son_deger));
+        }
+    }
+}
