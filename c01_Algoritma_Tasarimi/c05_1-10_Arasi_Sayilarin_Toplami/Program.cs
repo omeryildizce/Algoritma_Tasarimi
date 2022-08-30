711ac9c2fc72b0c8d98ad97ca2642c797545bfa2
omeryildizce <omeryildizce@gmail.com>
2022-08-30 14:56:58 +0300

    Sayılar Toplamı
    10'a ve n' e kadar olan sayıların toplamı yapıldı.

---
+++ c01_Algoritma_Tasarimi/c05_1-10_Arasi_Sayilarin_Toplami/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c05_1_10_Arasi_Sayilarin_Toplami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            One_to_ten_sum();
+
+            int sayi = 10000;
+            Toplam(sayi);
+        }
+
+        private static void Toplam(int sayi)
+        {
+            int toplam = 0;
+            toplam = (sayi * (sayi + 1)) / 2;
+            Console.WriteLine(toplam);
+        }
+
+        private static void One_to_ten_sum()
+        {
+            int sayi = 10;
+            int toplam = 0;
+            for (int i = 1; i <= sayi; i++)
+            {
+                toplam += i;
+            }
+
+            Console.WriteLine(toplam);
+        }
+    }
+
+
+}
