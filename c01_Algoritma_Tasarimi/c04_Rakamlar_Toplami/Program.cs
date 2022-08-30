cc6cb7712f19d6481dbe761a54f89e85bd01849b
omeryildizce <omeryildizce@gmail.com>
2022-08-30 14:44:10 +0300

    Rakamlar Toplamı Hesaplama

---
+++ c01_Algoritma_Tasarimi/c04_Rakamlar_Toplami/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c04_Rakamlar_Toplami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} sayısının rakamları toplamı = {1}",sayi,Rakamlar_Toplami.Rakamlar_Topla(sayi));
+        }
+    }
+}
---
+++ c01_Algoritma_Tasarimi/c04_Rakamlar_Toplami/Rakamlar_Toplami.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c04_Rakamlar_Toplami
+{
+    public class Rakamlar_Toplami
+    {
+        public static int Rakamlar_Topla(int sayi)
+        {
+            int result = 0;
+            int rakam = 0;
+            while (true)
+            {
+                if (sayi > 0)
+                {
+                    rakam = sayi % 10;
+                    sayi -= rakam;
+                    sayi /= 10;
+                    result += rakam;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
