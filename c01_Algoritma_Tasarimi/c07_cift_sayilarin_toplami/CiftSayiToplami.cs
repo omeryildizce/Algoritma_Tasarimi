b39aad60712058d781cba829aff37263f6b63c06
omeryildizce <omeryildizce@gmail.com>
2022-08-30 18:44:57 +0300

    Çift sayıların toplamı

---
+++ c01_Algoritma_Tasarimi/c07_cift_sayilarin_toplami/CiftSayiToplami.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c07_cift_sayilarin_toplami
+{
+    public class CiftSayiToplami
+    {
+        public static int ciftSayilariTopla(int x)
+        {
+            int toplam = 0;
+            for (int i = 1; i <= x; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    toplam += i;
+                }
+            }
+            return toplam;
+        }
+        public static int ciftSayilariTopla1 (int x)
+        {
+            int toplam = 0;
+            for (int i = 0; i <= x; i+=2)
+            {
+                toplam += i;
+            }
+            return toplam;
+        }
+        public static int ciftSayilariTopla2(int x)
+        {
+            int n = x / 2;
+            return n * (n + 1);
+        }
+    }
+}
---
+++ c01_Algoritma_Tasarimi/c07_cift_sayilarin_toplami/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c07_cift_sayilarin_toplami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int sayi = 13;
+          
+           Console.WriteLine(CiftSayiToplami.ciftSayilariTopla(sayi));
+           Console.WriteLine(CiftSayiToplami.ciftSayilariTopla1(sayi));
+           Console.WriteLine(CiftSayiToplami.ciftSayilariTopla2(sayi));
+
+        }
+    }
+}
