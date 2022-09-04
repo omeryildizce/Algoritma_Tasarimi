394331ae9fa07126b6841269302c69a49b15b237
omeryildizce <omeryildizce@gmail.com>
2022-09-04 14:25:02 +0300

    Tek boyutlu dizi

---
+++ c01_Algoritma_Tasarimi/c14_tek_boyutlu_dizi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace c14_tek_boyutlu_dizi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1D Array
+            int[] array = new int[5];
+
+            // get lenght of the array
+            int n = array.Length;
+
+            // random number generator for the array
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = new Random().Next(0,100);
+                Console.WriteLine(array[i]);
+                Thread.Sleep(1);
+
+            }
+        }
+    }
+}
