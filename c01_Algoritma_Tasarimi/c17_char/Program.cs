19e82b562c671563baf2f0dd0fc0de3ae14a6c19
omeryildizce <omeryildizce@gmail.com>
2022-09-04 17:49:25 +0300

    ASCII characters

---
+++ c01_Algoritma_Tasarimi/c17_char/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c17_char
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 65; i <= 90; i++)
+            {
+                Console.Write("{0,-2}", (char)i);
+            }
+            Console.WriteLine();
+            for (int i = 32; i <= 126; i++)
+            {
+               
+                if(i %32 ==0)
+                    Console.WriteLine();
+                Console.Write("{0,-2}", (char)i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
