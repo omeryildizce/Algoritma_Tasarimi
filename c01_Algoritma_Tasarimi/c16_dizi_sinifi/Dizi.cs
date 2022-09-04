7b48a824f369a79931e93ddb124d7811d08a2471
omeryildizce <omeryildizce@gmail.com>
2022-09-04 15:04:50 +0300

    Dizi Sınıfı Oluşturuldu

---
+++ c01_Algoritma_Tasarimi/c16_dizi_sinifi/Dizi.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace c16_dizi_sinifi
+{
+    public class Dizi
+    {
+        public int[] Olustur(int limit)
+        {
+            int[] sayilar = new int[limit];
+
+            for (int i = 0; i < limit; i++)
+            {
+                sayilar[i] = new Random().Next(1, 101);
+                Console.Write("{0,5}", sayilar[i]);
+                Thread.Sleep(1);
+            }
+            Console.WriteLine();
+            return sayilar;
+        }
+
+        public static int EnBuyuk(int[] dizi)
+        {
+            int enBuyuk = dizi[0];
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                if (dizi[i] > enBuyuk)
+                {
+                    enBuyuk = dizi[i];
+                }
+            }
+
+            return enBuyuk;
+        }
+        public static int EnKucuk(int[] dizi)
+        {
+            int enKucuk = dizi[0];
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                if (dizi[i] < enKucuk)
+                {
+                    enKucuk = dizi[i];
+                }
+            }
+
+            return enKucuk;
+        }
+    }
+}
---
+++ c01_Algoritma_Tasarimi/c16_dizi_sinifi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace c16_dizi_sinifi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("n değerini giriniz :");
+            int limit = Convert.ToInt32(Console.ReadLine());
+            int[] sayilar = new Dizi().Olustur(limit);
+
+
+            int enBuyuk = Dizi.EnBuyuk(sayilar);
+            Console.WriteLine("Dizinin en büyük elamanı: {0}", enBuyuk);
+            int enKucuk = Dizi.EnKucuk(sayilar);
+            Console.WriteLine("Dizinin en küçük elamanı: {0}", enKucuk);
+        }
+    }
+}
