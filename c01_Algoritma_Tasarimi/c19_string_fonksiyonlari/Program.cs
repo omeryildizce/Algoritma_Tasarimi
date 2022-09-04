4f7e54d70a1449ce1eb69bef04e8788310f4111d
omeryildizce <omeryildizce@gmail.com>
2022-09-04 19:35:39 +0300

    String Fonksiyonları

---
+++ c01_Algoritma_Tasarimi/c19_string_fonksiyonlari/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c19_string_fonksiyonlari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ifade = "   ÖmEr yIlDıZ   ";
+            Console.WriteLine(ifade);
+            Console.WriteLine(ifade.Length);
+            Console.WriteLine(ifade.Trim());
+            Console.WriteLine(ifade.TrimStart());
+            Console.WriteLine(ifade.TrimEnd());
+            Console.WriteLine(ifade.ToLower());
+            Console.WriteLine(ifade.ToUpper());
+            Console.WriteLine(ifade.Replace("E", "e"));
+
+
+
+        }
+    }
+}
