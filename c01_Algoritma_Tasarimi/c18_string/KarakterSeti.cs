84cd8430cacca1cef83bd7bf1c4c8ef885ca8fae
omeryildizce <omeryildizce@gmail.com>
2022-09-04 19:22:53 +0300

    String işlemleri yapıldı

---
+++ c01_Algoritma_Tasarimi/c18_string/KarakterSeti.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c18_string
+{
+    public class KarakterSeti
+    {
+        public static void karakterSeti(int baslangic = 65, int son = 90, int satirKarakter = 10)
+        {
+            for(int i = baslangic; i < son; i++)
+            {
+                Console.Write("{0,5}", (char)i);
+                if ((i-baslangic) % satirKarakter == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ c01_Algoritma_Tasarimi/c18_string/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c18_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(new string('-', i));
+
+            }
+
+            KarakterSeti.karakterSeti();
+            KarakterSeti.karakterSeti(128,255);
+            KarakterSeti.karakterSeti(128,255,20);
+
+
+        }
+    }
+}
