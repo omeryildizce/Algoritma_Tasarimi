a126f8aea10435d05c52b90fca1371ba9e8378d8
omeryildizce <omeryildizce@gmail.com>
2022-12-05 18:30:41 +0300

    collection

---
+++ c04_OOP/c06_Collection/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c06_Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // enumarator__();
+            Collection<int> a;
+            ICollection b;
+            ICollection<int> c;
+        }
+
+        private static void enumarator__()
+        {
+            string s = "Merhaba";
+            IEnumerator rator = s.GetEnumerator();
+            while (rator.MoveNext())
+            {
+                Console.WriteLine(rator.Current);
+            }
+
+            foreach (char c in s)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+    internal class MyGenCollection : IEnumerable<int>
+    {
+        int[] data = { 1, 2, 3 };
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            foreach (int i in data)
+            {
+                yield return i;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            foreach (int i in data)
+            {
+                yield return i;
+            }
+        }
+    }
+
+    internal class MyCollection : IEnumerable
+    {
+        int[] data = { 1, 2, 3 };
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (int i in data)
+            {
+                yield return i;
+            }
+        }
+    }
+}
